26db36e4aed197c2fd8d76af4431a0209446ad8e
gaurangkeluskar22 <gaurangkeluskar22@gmail.com>
2023-07-06 17:29:06 +0530

    added code for map

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import './App.css';
-import { useGetAllProductsQuery } from './store/productsApi';
+import "./App.css";
+import { useGetAllProductsQuery } from "./store/productsApi";
 
 function App() {
-  const {data} = useGetAllProductsQuery();
+  const { data } = useGetAllProductsQuery();
   console.log(data);
   return (
     <div className="App">
-      
+      {data?.products ? (
+        data.products.map((product) => {
+          return (
+            <div key={product.id} className="card">
+              <img src={product.images[0]} width="300px"></img>
+              <p>{product.brand}</p>
+              <p>{product.description}</p>
+            </div>
+          );
+        })
+      ) : (
+        <h1>loading</h1>
+      )}
     </div>
   );
 }
