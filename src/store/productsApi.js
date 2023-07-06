cf12755dbbf3fc4969fac91a879acdae0ef4a9a2
gaurangkeluskar22 <gaurangkeluskar22@gmail.com>
2023-07-06 16:50:41 +0530

    added apiprovider code

---
+++ src/store/productsApi.js
@@ -0,0 +1,14 @@
+import {createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export default productsApi = createApi({
+    reducerPath: "productsApi",
+    baseQuery : fetchBaseQuery({baseUrl : "https://dummyjson.com/"}),
+    endpoints : (builder) => {{
+        getAllProducts : builder.query({
+            query: () => "products",
+        })
+    }}
+});
+
+
+export const { useGetAllProductsQuery } = productsApi;
